9c0f25d8f01cb0e4512adcbf61e8a6fe70087ec8
Shridhar Pasodi <shridhar.pasodi.developer@gmail.com>
2015-06-22 14:26:42 +0530

    updating code for pull request

---
+++ SavingAccountApplication/SavingAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Banking;
+
+namespace SavingAccountApplication
+{
+    class SavingAccount : IBankAccount
+    {
+        public string AccountId { get; set; }
+        public float Balance { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public void Debit(float amount)
+        {
+            Console.WriteLine("Amount {0} has been debited.", amount);
+        }
+
+        public void Credit(float amount)
+        {
+            Console.WriteLine("Amount {0} has been credited.", amount);
+        }
+
+        public void CalculateInterest()
+        {
+            Console.WriteLine("Interest Calculated");
+        }
+
+        public void BalanceEnquiry()
+        {
+            Console.WriteLine("Balance Enquiry");
+        }
+    }
+}
