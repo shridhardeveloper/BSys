ecea88a4cbbd837391682a054db78c378896e2be
Shridhar Pasodi <shridhar.pasodi.developer@gmail.com>
2015-06-22 16:13:36 +0530

    checkin to rebase

--- SavingAccountApplication/Program.cs
+++ SavingAccountApplication/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Autofac;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Banking;
 
 namespace SavingAccountApplication
 {
@@ -10,6 +12,14 @@ namespace SavingAccountApplication
     {
         static void Main(string[] args)
         {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<SavingAccount>().As<IBankAccount>();
+            builder.RegisterType<User>().As<IUser>();
+            var container = builder.Build();
+
+            container.Resolve<IBankAccount>().BalanceEnquiry();
+            //container.Resolve<IUser>().GetProfile();
+            Console.ReadLine();  
         }
     }
 }
